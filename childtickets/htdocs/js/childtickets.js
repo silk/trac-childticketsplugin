f6d70854593ff5625563d773ecd3ac90783d4063
Grzegorz Sobański <grzegorz.sobanski@mlabs.pl>
2011-10-11 15:13:04 +0200

    use a safe version of jquery() instead of

--- childtickets/htdocs/js/childtickets.js
+++ childtickets/htdocs/js/childtickets.js
@@ -1,7 +1,8 @@
-$(function() {
+jQuery(document).ready(function($) {
     $("#cb_show_closed").click(
         function() {
             $("tr.closed").toggle(this.checked)
         })
     $("tr.closed").toggle($("#cb_show_closed").checked())
 })
+
